7e8119eb490ebd484ab33268a7fbd1ad42db04b4
escidron <eduardosanchezcidron@gmail.com>
2023-07-29 01:29:28 -0300

    fix my booking cards

--- sections/myAdex/MyBookings.jsx
+++ sections/myAdex/MyBookings.jsx
@@ -1,28 +1,32 @@
 'use client'
 import { useState } from 'react'
 import Card from './Card'
+import Link from 'next/link'
 
-export default function MyBookings({data}) {
-  if(data.length === 0){
-    return(
+export default function MyBookings({ data }) {
+  if (data.length === 0) {
+    return (
       <>
-      <h1 className='text-[20px]'>There are not exist any Booking yet</h1>
-      <p className='text-gray-600'>Go to market place for getting your first one.</p>
+        <h1 className='text-[20px]'>There are not exist any Booking yet</h1>
+        <p className='text-gray-600'>Go to market place for getting your first one.</p>
       </>
     )
   }
 
   return (
-        <div className='flex flex-col items-center justify-center mt-8'>
+    <div className='flex flex-col items-center justify-center mt-8'>
       {
-        data.map((item)=>{
-          return(
+        data.map((item) => {
+          return (
             <section key={item.id} className='w-full justify-center'>
-              <Card item={item}/>
+              <Link href={`/my-booking?id=${item.id}`}
+                className='cursor-pointer'>
+                <Card item={item}  />
+              </Link>
               <div className='w-[90%] h-[1px] mx-auto bg-gray-200 mt-8 mb-8'></div>
             </section>
           )
-       })
+        })
       }
 
     </div>
