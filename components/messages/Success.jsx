8a3d400b89e404fab148c1ca5a8bfc5b78053845
escidron <eduardosanchezcidron@gmail.com>
2023-07-29 00:33:57 -0300

    fix the align of de succes message box

--- components/messages/Success.jsx
+++ components/messages/Success.jsx
@@ -3,7 +3,7 @@ import BlackButton from '../buttons/BlackButton';
 import Link from 'next/link';
 export default function Success({ children }) {
     return (
-        <div className='relative border border-green-700 rounded-xl p-8  w-full max-w-[500px] shadow-md'>
+        <div className='relative border border-green-700 rounded-xl p-8  w-full max-w-[500px] shadow-md mx-auto'>
             <div className='absolute top-[-25px] left-[calc(50%-25px)] bg-white '>
                 <CheckCircleIcon  sx={{ fontSize: "50px",color:'green' }}/>
             </div>
