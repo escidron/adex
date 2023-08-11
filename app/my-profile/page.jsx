c7ca9d6b8410c0b8723fd6f331d446a9e183a6cc
escidron <eduardosanchezcidron@gmail.com>
2023-08-10 22:56:48 -0300

    add the logic for rendering the tab My companies

--- app/my-profile/page.jsx
+++ app/my-profile/page.jsx
@@ -1,13 +1,32 @@
 
 "use client"
-import React,{useState} from 'react'
+import { useEffect,useState } from 'react';
 import FolderComponent from '@/components/folder/FolderComponent'
 
 
-export default function page() {
+export default function Profilepage() {
+  const [userData, setUserData] = useState({});
+
+  useEffect(() => {
+    async function GetUserProfile() {
+        const response = await fetch(
+            "http://localhost:5000/api/users/user-profile",
+            {
+                method: "GET",
+                credentials: "include",
+            }
+        );
+        if (response.status === 200) {
+            const res = await response.json()
+            setUserData(res)
+        }
+    }
+    GetUserProfile();
+}, []);
+
   return (
         <main> 
-            <FolderComponent />
+            <FolderComponent userType={userData.user_type}/>
         </main>
   )
 }
--- components/folder/FolderComponent.jsx
+++ components/folder/FolderComponent.jsx
@@ -11,11 +11,9 @@ import MyAdex from '@/sections/myAdex/MyAdex';
 import MyCompanies from '@/sections/companies/MyCompanies'
 
 const inter = Inter({ subsets: ['latin'] })
-export default function FolderComponent() {
-    const [folder, setFolder] = useState(1);
+export default function FolderComponent({ userType }) {
     const searchParams = useSearchParams()
     const tab = searchParams.get('tab')
-    console.log('tab', tab)
     return (
         <>
             <div className='flex justify-center bg-[#D9D9D9] h-[40px]'>
@@ -24,7 +22,11 @@ export default function FolderComponent() {
                 <Link href='my-profile?tab=3' className={`border-l-[1px] border-r-[1px] ${tab == 3 ? 'bg-[#FCD33B]' : 'bg-white'} px-[40px] text-[15px] ${inter.className} font-[600] flex items-center justify-center hover:bg-[#FCD33B]`}>Notifications</Link>
                 <Link href='my-profile?tab=4' className={`border-l-[1px] border-r-[1px] ${tab == 4 ? 'bg-[#FCD33B]' : 'bg-white'} px-[40px] text-[15px] ${inter.className} font-[600] flex items-center justify-center hover:bg-[#FCD33B]`}>My Wallet</Link>
                 <Link href='my-profile?tab=5' className={`border-l-[1px] border-r-[1px] ${tab == 5 ? 'bg-[#FCD33B]' : 'bg-white'} px-[40px] text-[15px] ${inter.className} font-[600] flex items-center justify-center hover:bg-[#FCD33B]`}>My ADEX</Link>
-                <Link href='my-profile?tab=6' className={`border-l-[1px] border-r-[1px] ${tab == 6 ? 'bg-[#FCD33B]' : 'bg-white'} px-[40px] text-[15px] ${inter.className} font-[600] flex items-center justify-center hover:bg-[#FCD33B]`}>My Companies</Link>
+                {
+                    userType == '1' && (
+                        <Link href='my-profile?tab=6' className={`border-l-[1px] border-r-[1px] ${tab == 6 ? 'bg-[#FCD33B]' : 'bg-white'} px-[40px] text-[15px] ${inter.className} font-[600] flex items-center justify-center hover:bg-[#FCD33B]`}>My Companies</Link>
+                    )
+                }
             </div>
             {tab == 1 && (<PersonalInfo />)}
             {tab == 2 && (<Security />)}
