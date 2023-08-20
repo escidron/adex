c7ebb88349999cb1c5664792ae3c899c3c50d927
escidron <eduardosanchezcidron@gmail.com>
2023-08-20 12:44:49 -0300

    add dropdown component

---
+++ components/dropdown/DropdownButton.jsx
@@ -0,0 +1,35 @@
+import * as React from 'react';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+
+export default function DropDownButton({ label,itens,dropDownSelected,setDropDownSelected }) {
+
+  const handleChange = (event) => {
+    setDropDownSelected(event.target.value);
+  };
+  return (
+    <FormControl sx={{ minWidth: 120,width:'100%',height:'100%' }} size="small">
+      <InputLabel id="demo-select-small-label" sx={{top:dropDownSelected != ''?0:'8px'}}>{label}</InputLabel>
+      <Select
+        labelId="demo-select-small-label"
+        id="demo-select-small"
+        value={dropDownSelected}
+        label={label}
+        onChange={handleChange}
+        sx={{height:'100%'}}
+      >
+        {
+            itens.map((item)=>(
+                
+                <MenuItem value={item.id} key={item.id}>{item.name}</MenuItem>
+            ))
+        }
+        <MenuItem value={99}>
+          Other
+        </MenuItem>
+      </Select>
+    </FormControl>
+  );
+}
