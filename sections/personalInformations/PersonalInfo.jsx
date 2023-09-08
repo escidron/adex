bfc19989b92307733cb1781f8d4d5d6f5e37f680
escidron <eduardosanchezcidron@gmail.com>
2023-09-07 22:36:55 -0300

    add the is public information logic

--- sections/personalInformations/PersonalInfo.jsx
+++ sections/personalInformations/PersonalInfo.jsx
@@ -1,13 +1,18 @@
 "use client"
 
-import { useState,useEffect } from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import { Inter } from 'next/font/google'
 import EditIcon from '@mui/icons-material/Edit';
-import axios from 'axios';
+import VisibilityIcon from '@mui/icons-material/Visibility';
+import VisibilityOffIcon from '@mui/icons-material/VisibilityOff';
+
 const inter = Inter({ subsets: ['latin'] })
 
 export default function PersonalInfo() {
   const [currentInfo, setCurrentInfo] = useState('');
+  const [refresh, setRefresh] = useState(false)
+
   const [user, setUser] = useState({
     name: '',
     lastName: '',
@@ -16,7 +21,12 @@ export default function PersonalInfo() {
     bio: '',
     profession: '',
     sex: '',
-    handle:''
+    handle: '',
+    handleIsPublic: '0',
+    professionIsPublic: '0',
+    sexIsPublic: '0',
+    bioIsPublic: '0'
+
   });
   useEffect(() => {
     async function GetUserProfile() {
@@ -34,40 +44,75 @@ export default function PersonalInfo() {
       }
     }
     GetUserProfile();
-  }, []);
+  }, [refresh]);
 
   const handleSex = (e) => {
     const id = e.currentTarget.id
     if (id !== user.sex) {
-      setUser({...user,sex:id})
+      setUser({ ...user, sex: id })
     } else {
-      setUser({...user,sex:''})
+      setUser({ ...user, sex: '' })
     }
   }
 
-  const submit = ()=>{
+  const submit = () => {
     setCurrentInfo('')
     axios.post('https://test.adexconnect.com/api/users/update-user-profile',
-    { 
-      name: user.name,
-      lastName: user.lastName,
-      email: user.email,
-      phone: user.phone,
-      bio: user.bio,
-      profession: user.profession,
-      sex: user.sex,
-      handle:user.handle
-    
-    }, {
-    withCredentials: true,
-  })
-    .then(function (response) {
-      console.log('response',response)
+      {
+        name: user.name,
+        lastName: user.lastName,
+        email: user.email,
+        phone: user.phone,
+        bio: user.bio,
+        profession: user.profession,
+        sex: user.sex,
+        handle: user.handle,
+        handleIsPublic:user.handleIsPublic,
+        professionIsPublic:user.professionIsPublic,
+        sexIsPublic:user.sexIsPublic,
+        bioIsPublic:user.bioIsPublic
+
+      }, {
+      withCredentials: true,
     })
-    .catch(function (error) {
-      console.log(error)
-    });
+      .then(function (response) {
+        console.log('response', response)
+      })
+      .catch(function (error) {
+        console.log(error)
+      });
   }
+
+  const handlePublicInfo = (info) => {
+    axios.post('https://test.adexconnect.com/api/users/update-user-profile',
+      {
+        name: user.name,
+        lastName: user.lastName,
+        email: user.email,
+        phone: user.phone,
+        bio: user.bio,
+        profession: user.profession,
+        sex: user.sex,
+        handle: user.handle,
+        handleIsPublic:info=='handle' && user.handleIsPublic == '1'?'0':info=='handle' && user.handleIsPublic == '0'?'1':user.handleIsPublic,
+        professionIsPublic:info=='profession' && user.professionIsPublic == '1'?'0':info=='profession' && user.professionIsPublic == '0'?'1':user.professionIsPublic,
+        sexIsPublic:info=='sex' && user.sexIsPublic == '1'?'0':info=='sex' && user.sexIsPublic == '0'?'1':user.sexIsPublic,
+        bioIsPublic:info=='bio' && user.bioIsPublic == '1'?'0':info=='bio' && user.bioIsPublic == '0'?'1':user.bioIsPublic
+
+
+      }, {
+      withCredentials: true,
+    })
+      .then(function (response) {
+        console.log('response', response)
+        
+      })
+      .catch(function (error) {
+        console.log(error)
+      });
+      setRefresh(!refresh)
+  }
+
   return (
     <div className={` flex flex-col items-center  min-h-screen py-2 `}>
       <div className="
@@ -91,8 +136,9 @@ export default function PersonalInfo() {
                     type="text"
                     id="name"
                     name="name"
-                    value={user.name != 'null' ?user.name:''}
-                    onChange={(e)=>setUser({...user,name:e.target.value})}
+                    placeholder='Name'
+                    value={user.name != 'null' ? user.name : ''}
+                    onChange={(e) => setUser({ ...user, name: e.target.value })}
                     className="w-full border text-black p-3 max-h-[42px] rounded-lg outline-none focus:border-black"
                   />
                   <button onClick={submit} className='style_banner_button  mx-auto z-10 bg-black py-[4px] px-[20px] h-10 rounded-md  hover:bg-[#FCD33B] hover:text-black text-lg transition ease-linear duration-200'>
@@ -103,7 +149,7 @@ export default function PersonalInfo() {
                 <>
                   <div>
                     <p className='font-[600]'>Name</p>
-                    <p className='font-[300] text-gray-600'>{user.name != 'null' && user.name?user.name:'Not Provided'}</p>
+                    <p className='font-[300] text-gray-600'>{user.name != 'null' && user.name ? user.name : 'Not Provided'}</p>
                   </div>
                   <div onClick={() => setCurrentInfo('name')} className='flex gap-1 rounded-xl border items-center justify-between py-2 px-4 cursor-pointer hover:border-black'>
                     <p className='text-gray-600 '>Edit</p>
@@ -122,8 +168,9 @@ export default function PersonalInfo() {
                     type="text"
                     id="lastname"
                     name="lastname"
-                    value={user.lastName != 'null' ?user.lastName:''}
-                    onChange={(e)=>setUser({...user,lastName:e.target.value})}
+                    placeholder='Last Name'
+                    value={user.lastName != 'null' ? user.lastName : ''}
+                    onChange={(e) => setUser({ ...user, lastName: e.target.value })}
                     className="w-full border text-black p-3 max-h-[42px] rounded-lg outline-none focus:border-black"
                   />
                   <button onClick={submit} className='style_banner_button  mx-auto z-10 bg-black py-[4px] px-[20px] h-10 rounded-md  hover:bg-[#FCD33B] hover:text-black text-lg transition ease-linear duration-200'>
@@ -134,7 +181,7 @@ export default function PersonalInfo() {
                 <>
                   <div>
                     <p className='font-[600]'>Last Name</p>
-                    <p className='font-[300] text-gray-600'>{user.lastName != 'null' && user.lastName?user.lastName:'Not Provided'}</p>
+                    <p className='font-[300] text-gray-600'>{user.lastName != 'null' && user.lastName ? user.lastName : 'Not Provided'}</p>
                   </div>
                   <div onClick={() => setCurrentInfo('lastname')} className='flex gap-1 rounded-xl border items-center justify-between py-2 px-4 cursor-pointer hover:border-black'>
                     <p className='text-gray-600 '>Edit</p>
@@ -168,15 +215,27 @@ export default function PersonalInfo() {
                     <p className='font-[600]'>User Handle</p>
                     <p className='font-[300] text-gray-600'>{user.handle != 'null' && user.handle ? user.handle : 'Not Provided'}</p>
                   </div>
-                  <div onClick={() => setCurrentInfo('handle')} className='flex gap-1 rounded-xl border items-center justify-between py-2 px-4 cursor-pointer hover:border-black'>
-                    <p className='text-gray-600 '>Edit</p>
-                    <EditIcon sx={{ fontSize: '16px', color: 'gray' }} />
+                  <div className='flex gap-2'>
+                    <div onClick={() => setCurrentInfo('handle')} className='flex gap-1 rounded-xl border items-center justify-between py-2 px-4 cursor-pointer hover:border-black'>
+                      <p className='text-gray-600 '>Edit</p>
+                      <EditIcon sx={{ fontSize: '16px', color: 'gray' }} />
+                    </div>
+                    <div className="flex gap-1 rounded-xl border items-center justify-between py-2 px-2 cursor-pointer hover:border-black" onClick={() => handlePublicInfo('handle')}>
+                      {
+                        user.handleIsPublic == '1' ? (
+
+                          <VisibilityIcon fontSize="small" sx={{ opacity: '0.6' }} />
+                        ) : (
+                          <VisibilityOffIcon fontSize="small" sx={{ opacity: '0.6' }} />
+                        )
+                      }
+                    </div>
                   </div>
                 </>
               )
             }
           </div>
-          
+
           <div className="border rounded-md py-3 px-4 flex justify-between items-center gap-4 min-h-[74px]">
             {
               currentInfo === 'email' ? (
@@ -185,8 +244,9 @@ export default function PersonalInfo() {
                     type="text"
                     id="email"
                     name="email"
-                    value={user.email != 'null' ?user.email:''}
-                    onChange={(e)=>setUser({...user,email:e.target.value})}
+                    placeholder='Email'
+                    value={user.email != 'null' ? user.email : ''}
+                    onChange={(e) => setUser({ ...user, email: e.target.value })}
                     className="w-full border text-black p-3 max-h-[42px] rounded-lg outline-none focus:border-black"
                   />
                   <button onClick={submit} className='style_banner_button  mx-auto z-10 bg-black py-[4px] px-[20px] h-10 rounded-md  hover:bg-[#FCD33B] hover:text-black text-lg transition ease-linear duration-200'>
@@ -197,7 +257,7 @@ export default function PersonalInfo() {
                 <>
                   <div>
                     <p className='font-[600]'>Email</p>
-                    <p className='font-[300] text-gray-600'>{user.email != 'null' && user.email?user.email:'Not Provided'}</p>
+                    <p className='font-[300] text-gray-600'>{user.email != 'null' && user.email ? user.email : 'Not Provided'}</p>
                   </div>
                   <div onClick={() => setCurrentInfo('email')} className='flex gap-1 rounded-xl border items-center justify-between py-2 px-4 cursor-pointer hover:border-black'>
                     <p className='text-gray-600 '>Edit</p>
@@ -216,8 +276,9 @@ export default function PersonalInfo() {
                     type="text"
                     id="phone"
                     name="phone"
-                    value={user.phone != 'null' ?user.phone:''}
-                    onChange={(e)=>setUser({...user,phone:e.target.value})}
+                    placeholder='Phone'
+                    value={user.phone != 'null' ? user.phone : ''}
+                    onChange={(e) => setUser({ ...user, phone: e.target.value })}
                     className="w-full border text-black p-3 max-h-[42px] rounded-lg outline-none focus:border-black"
                   />
                   <button onClick={submit} className='style_banner_button  mx-auto z-10 bg-black py-[4px] px-[20px] h-10 rounded-md  hover:bg-[#FCD33B] hover:text-black text-lg transition ease-linear duration-200'>
@@ -228,7 +289,7 @@ export default function PersonalInfo() {
                 <>
                   <div>
                     <p className='font-[600]'>Phone Number</p>
-                    <p className='font-[300] text-gray-600'>{user.phone != 'null' && user.phone?user.phone:'Not Provided'}</p>
+                    <p className='font-[300] text-gray-600'>{user.phone != 'null' && user.phone ? user.phone : 'Not Provided'}</p>
                   </div>
                   <div onClick={() => setCurrentInfo('phone')} className='flex gap-1 rounded-xl border items-center justify-between py-2 px-4 cursor-pointer hover:border-black'>
                     <p className='text-gray-600 '>Edit</p>
@@ -238,7 +299,6 @@ export default function PersonalInfo() {
               )
             }
           </div>
-
           <div className="border rounded-md py-3 px-4 flex justify-between items-center gap-4 min-h-[74px] ">
             {
               currentInfo === 'profession' ? (
@@ -262,9 +322,21 @@ export default function PersonalInfo() {
                     <p className='font-[600]'>Profession</p>
                     <p className='font-[300] text-gray-600'>{user.profession != 'null' && user.profession ? user.profession : 'Not Provided'}</p>
                   </div>
-                  <div onClick={() => setCurrentInfo('profession')} className='flex gap-1 rounded-xl border items-center justify-between py-2 px-4 cursor-pointer hover:border-black'>
-                    <p className='text-gray-600 '>Edit</p>
-                    <EditIcon sx={{ fontSize: '16px', color: 'gray' }} />
+                  <div className='flex gap-2'>
+                    <div onClick={() => setCurrentInfo('profession')} className='flex gap-1 rounded-xl border items-center justify-between py-2 px-4 cursor-pointer hover:border-black'>
+                      <p className='text-gray-600 '>Edit</p>
+                      <EditIcon sx={{ fontSize: '16px', color: 'gray' }} />
+                    </div>
+                    <div className="flex gap-1 rounded-xl border items-center justify-between py-2 px-2 cursor-pointer hover:border-black" onClick={() => handlePublicInfo('profession')}>
+                      {
+                        user.professionIsPublic == '1' ? (
+
+                          <VisibilityIcon fontSize="small" sx={{ opacity: '0.6' }} />
+                        ) : (
+                          <VisibilityOffIcon fontSize="small" sx={{ opacity: '0.6' }} />
+                        )
+                      }
+                    </div>
                   </div>
                 </>
               )
@@ -310,29 +382,41 @@ export default function PersonalInfo() {
                 <>
                   <div>
                     <p className='font-[600]'>Sex</p>
-                    <p className='font-[300] text-gray-600'>{user.sex != 'null' && user.sex ? (user.sex ==1?'Male':user.sex == 2?'Female':user.sex == 3?'Other':'') : 'Not Provided'}</p>
+                    <p className='font-[300] text-gray-600'>{user.sex != 'null' && user.sex ? (user.sex == 1 ? 'Male' : user.sex == 2 ? 'Female' : user.sex == 3 ? 'Other' : '') : 'Not Provided'}</p>
                   </div>
-                  <div onClick={() => setCurrentInfo('sex')} className='flex gap-1 rounded-xl border items-center justify-between py-2 px-4 cursor-pointer hover:border-black'>
-                    <p className='text-gray-600 '>Edit</p>
-                    <EditIcon sx={{ fontSize: '16px', color: 'gray' }} />
+                  <div className='flex gap-2'>
+                    <div onClick={() => setCurrentInfo('sex')} className='flex gap-1 rounded-xl border items-center justify-between py-2 px-4 cursor-pointer hover:border-black'>
+                      <p className='text-gray-600 '>Edit</p>
+                      <EditIcon sx={{ fontSize: '16px', color: 'gray' }} />
+                    </div>
+                    <div className="flex gap-1 rounded-xl border items-center justify-between py-2 px-2 cursor-pointer hover:border-black" onClick={() => handlePublicInfo('sex')}>
+                      {
+                        user.sexIsPublic == '1' ? (
+
+                          <VisibilityIcon fontSize="small" sx={{ opacity: '0.6' }} />
+                        ) : (
+                          <VisibilityOffIcon fontSize="small" sx={{ opacity: '0.6' }} />
+                        )
+                      }
+                    </div>
                   </div>
                 </>
               )
             }
           </div>
 
-          <div className="border rounded-md py-3 px-4 flex justify-between items-center gap-4 min-h-[74px]">
+          <div className="border rounded-md py-3 px-4 flex justify-between items-center gap-4 min-h-[74px] h-auto">
             {
               currentInfo === 'bio' ? (
                 <>
-                  <input
+                  <textarea
                     type="text"
                     id="bio"
                     name="bio"
                     placeholder='About you'
                     value={user.bio != 'null' ? user.bio : ''}
                     onChange={(e) => setUser({ ...user, bio: e.target.value })}
-                    className="w-full border text-black p-3 max-h-[42px] rounded-lg outline-none focus:border-black"
+                    className="w-full border text-black p-3 min-h-[200px] rounded-lg outline-none focus:border-black"
                   />
                   <button onClick={submit} className='style_banner_button  mx-auto z-10 bg-black py-[4px] px-[20px] h-10 rounded-md  hover:bg-[#FCD33B] hover:text-black text-lg transition ease-linear duration-200'>
                     <p className='style_banner_button_text font-medium '>Save</p>
@@ -344,9 +428,21 @@ export default function PersonalInfo() {
                     <p className='font-[600]'>Who I Am</p>
                     <p className='font-[300] text-gray-600'>{user.bio != 'null' && user.bio ? user.bio : 'Not Provided'}</p>
                   </div>
-                  <div onClick={() => setCurrentInfo('bio')} className='flex gap-1 rounded-xl border items-center justify-between py-2 px-4 cursor-pointer hover:border-black'>
-                    <p className='text-gray-600 '>Edit</p>
-                    <EditIcon sx={{ fontSize: '16px', color: 'gray' }} />
+                  <div className='flex gap-2'>
+                    <div onClick={() => setCurrentInfo('bio')} className='flex gap-1 rounded-xl border items-center justify-between py-2 px-4 cursor-pointer hover:border-black'>
+                      <p className='text-gray-600 '>Edit</p>
+                      <EditIcon sx={{ fontSize: '16px', color: 'gray' }} />
+                    </div>
+                    <div className="flex gap-1 rounded-xl border items-center justify-between py-2 px-2 cursor-pointer hover:border-black" onClick={() => handlePublicInfo('bio')}>
+                      {
+                        user.bioIsPublic == '1' ? (
+
+                          <VisibilityIcon fontSize="small" sx={{ opacity: '0.6' }} />
+                        ) : (
+                          <VisibilityOffIcon fontSize="small" sx={{ opacity: '0.6' }} />
+                        )
+                      }
+                    </div>
                   </div>
                 </>
               )
@@ -355,6 +451,6 @@ export default function PersonalInfo() {
 
         </div>
       </div>
-    </div>
+    </div >
   );
 }
