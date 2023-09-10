3d4d425efeaf037104dc98e682c3709f8c324d5a
escidron <eduardosanchezcidron@gmail.com>
2023-09-10 13:34:06 -0300

    facebook integration 8

--- app/my-listing/sharing-listing/page.jsx
+++ app/my-listing/sharing-listing/page.jsx
@@ -11,7 +11,7 @@ export default function SharingPage() {
             <meta property="og:description" content="How much does culture influence creative thinking?" />
             <meta property="og:image" content="http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg" />
             <div className='mt-[120px]'>
-                <AdDetails />
+                {/* <AdDetails /> */}
             </div>
 
         </>
