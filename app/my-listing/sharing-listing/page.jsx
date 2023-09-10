8c7fd956fff1952af4725d1cbe51a3197275cb4f
escidron <eduardosanchezcidron@gmail.com>
2023-09-10 14:17:40 -0300

    facebook integration 9

--- app/my-listing/sharing-listing/page.jsx
+++ app/my-listing/sharing-listing/page.jsx
@@ -5,11 +5,11 @@ import React from 'react'
 export default function SharingPage() {
     return (
         <>
-            <meta property="og:url" content="https://adexconnect.com/my-listing/sharing-listing?id=265" />
+            <meta property="og:url" content="https://adexconnect.com/my-listing/sharing-listing" />
             <meta property="og:type" content="article" />
             <meta property="og:title" content="When Great Minds Don’t Think Alike" />
             <meta property="og:description" content="How much does culture influence creative thinking?" />
-            <meta property="og:image" content="http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg" />
+            <meta property="og:image" content="https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?cs=srgb&dl=pexels-pixabay-268533.jpg&fm=jpg" />
             <div className='mt-[120px]'>
                 {/* <AdDetails /> */}
             </div>
