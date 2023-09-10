7371893745e5420d96d771ae84a3d1454ea5ccd5
escidron <eduardosanchezcidron@gmail.com>
2023-09-10 01:54:10 -0300

    testing facebook integration

--- components/facebook/ShareButton.jsx
+++ components/facebook/ShareButton.jsx
@@ -1,11 +1,29 @@
-import { FacebookProvider, ShareButton,Page } from 'react-facebook';
+import { FacebookProvider, ShareButton, useShare, Page, Feed } from 'react-facebook';
+import Head from 'next/head';
 
 export default function ShareButtonFacebook() {
+
   return (
-    <FacebookProvider appId="834219211688728">
-      <ShareButton href="http://www.facebook.com" className="my-classname">
-      Facebook
-      </ShareButton>
-    </FacebookProvider>
+    <>
+      <Head>
+        {/* Open Graph Meta Tags */}
+        <meta property="og:url" content='http://localhost:3000' />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content='Your Page Title' />
+        <meta property="og:description" content='desciption' />
+        <meta property="og:image" content='https://media.istockphoto.com/id/1402485839/photo/green-blurred-nature-background.webp?b=1&s=612x612&w=0&k=20&c=f4HpyXYygEoZ3lmU6I8OF8RyuWfanZovKTFQNjOIlx4=' />
+        {/* Optional: Set the image dimensions for better rendering */}
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+      </Head>
+      <FacebookProvider appId="1611678826026608" >
+        <ShareButton
+          href="adexconnect.com"
+          className="my-classname"
+          >
+          Facebook
+        </ShareButton>
+      </FacebookProvider>
+    </>
   );
 }
