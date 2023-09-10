b948054b5bfd8d1ec0d440dc520f96f6dd4462d0
escidron <eduardosanchezcidron@gmail.com>
2023-09-10 13:29:59 -0300

    facebook integration 7

--- app/my-listing/sharing-listing/page.jsx
+++ app/my-listing/sharing-listing/page.jsx
@@ -1,15 +1,19 @@
 'use client'
+import AdDetails from '@/app/market-place/details/page'
 import React from 'react'
 
 export default function SharingPage() {
     return (
         <>
-            <meta property="og:url" content="http://www.nytimes.com/2015/02/19/arts/international/when-great-minds-dont-think-alike.html" />
+            <meta property="og:url" content="https://adexconnect.com/my-listing/sharing-listing?id=265" />
             <meta property="og:type" content="article" />
             <meta property="og:title" content="When Great Minds Don’t Think Alike" />
             <meta property="og:description" content="How much does culture influence creative thinking?" />
             <meta property="og:image" content="http://static01.nyt.com/images/2015/02/19/arts/international/19iht-btnumbers19A/19iht-btnumbers19A-facebookJumbo-v2.jpg" />
-            <div className='mt-[200px]'>SharingP</div>
+            <div className='mt-[120px]'>
+                <AdDetails />
+            </div>
+
         </>
     )
 }
--- components/facebook/ShareButton.jsx
+++ components/facebook/ShareButton.jsx
@@ -7,7 +7,7 @@ export default function ShareButtonFacebook() {
     <>
       <FacebookProvider appId="1611678826026608" >
         <ShareButton
-          href="https://adexconnect.com/my-listing/sharing-listing"
+          href="https://adexconnect.com/my-listing/sharing-listing?id=265"
           className="my-classname"
           >
           Facebook
