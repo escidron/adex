c21ed7a7f6dcbbead655bafb1dcf0889ceb6fbd3
escidron <eduardosanchezcidron@gmail.com>
2023-09-10 14:28:33 -0300

    fb integration 11

--- app/my-listing/sharing-listing/page.jsx
+++ app/my-listing/sharing-listing/page.jsx
@@ -6,9 +6,10 @@ export default function SharingPage() {
     return (
         <>
             <meta property="og:url" content="https://adexconnect.com/my-listing/sharing-listing" />
+            <meta property="fb:app_id" content="1611678826026608" />
             <meta property="og:type" content="article" />
-            <meta property="og:title" content="When Great Minds Don’t Think Alike" />
-            <meta property="og:description" content="How much does culture influence creative thinking?" />
+            <meta property="og:title" content="Name of the listing" />
+            <meta property="og:description" content="Las Piñas Greande Manila" />
             <meta property="og:image" content="https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?cs=srgb&dl=pexels-pixabay-268533.jpg&fm=jpg" />
             <div className='mt-[120px]'>
                 {/* <AdDetails /> */}
