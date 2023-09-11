d3565d9dde69b128dd969239ab85f46109390435
escidron <eduardosanchezcidron@gmail.com>
2023-09-10 23:27:19 -0300

    debugging

--- app/my-listing/sharing-listing/page.jsx
+++ app/my-listing/sharing-listing/page.jsx
@@ -29,8 +29,8 @@ export default async function SharingPage({searchParams}) {
                 <meta property="og:url" content="https://adexconnect.com/my-listing/sharing-listing" />
                 <meta property="fb:app_id" content="1611678826026608" />
                 <meta property="og:type" content="article" />
-                <meta property="og:title" content={data?.title} />
-                <meta property="og:description" content={data?.address} />
+                <meta property="og:title" content={data?.title || ''} />
+                <meta property="og:description" content={data?.address || ''} />
                 <meta property="og:image" content="https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?cs=srgb&dl=pexels-pixabay-268533.jpg&fm=jpg" />
                 <main>
                     <div className='mt-[120px]'>
