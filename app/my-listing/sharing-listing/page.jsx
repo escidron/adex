ede7e539b1875334d3209912da0c5f0a0cec0fb3
escidron <eduardosanchezcidron@gmail.com>
2023-09-10 21:30:57 -0300

    fb integration 14

--- app/my-listing/sharing-listing/page.jsx
+++ app/my-listing/sharing-listing/page.jsx
@@ -9,19 +9,19 @@ export default function SharingPage() {
         setRendered(true)
     }, []);
 
-    if (!rendered) {
-        return (
-            <>
-                <meta property="og:url" content="https://adexconnect.com/my-listing/sharing-listing" />
-                <meta property="fb:app_id" content="1611678826026608" />
-                <meta property="og:type" content="article" />
-                <meta property="og:title" content="Name of the listings" />
-                <meta property="og:description" content="Las Piñas Greande Manila" />
-                <meta property="og:image" content="https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?cs=srgb&dl=pexels-pixabay-268533.jpg&fm=jpg" />
+    // if (!rendered) {
+    //     return (
+    //         <>
+    //             <meta property="og:url" content="https://adexconnect.com/my-listing/sharing-listing" />
+    //             <meta property="fb:app_id" content="1611678826026608" />
+    //             <meta property="og:type" content="article" />
+    //             <meta property="og:title" content="Name of the listings" />
+    //             <meta property="og:description" content="Las Piñas Greande Manila" />
+    //             <meta property="og:image" content="https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?cs=srgb&dl=pexels-pixabay-268533.jpg&fm=jpg" />
 
-            </>
-        )
-    }
+    //         </>
+    //     )
+    // }
     return (
         <>
             <meta property="og:url" content="https://adexconnect.com/my-listing/sharing-listing" />
