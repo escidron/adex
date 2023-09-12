c8e086ac7802d25f4bf299f550abff27d59f140b
escidron <eduardosanchezcidron@gmail.com>
2023-09-11 23:49:12 -0300

    fix fb integration

--- components/facebook/ShareButton.jsx
+++ components/facebook/ShareButton.jsx
@@ -8,7 +8,7 @@ export default function ShareButtonFacebook({id}) {
     <>
       <FacebookProvider appId="1611678826026608" >
         <ShareButton
-          href={`adexconnect.com/my-listing/sharing-listing?id=${id}`}
+          href={`adexconnect.com/my-listing/sharing-listing/${id}`}
         >
           <div className='flex gap-3 border p-3 mt-2 bg-white shadow-sm rounded-lg cursor-pointer hover:border-black'>
             <FacebookIcon sx={{ color: 'blue' }} />
