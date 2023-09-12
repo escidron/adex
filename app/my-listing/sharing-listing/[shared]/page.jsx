0fee3a7ef1cc2b4820b8dd3e237d1026494cadf8
escidron <eduardosanchezcidron@gmail.com>
2023-09-11 23:15:52 -0300

    fixing fb integration

--- app/my-listing/sharing-listing/[shared]/page.jsx
+++ app/my-listing/sharing-listing/[shared]/page.jsx
@@ -34,8 +34,7 @@ export default async function SharingPage({params}) {
                 <meta property="og:image" content="https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?cs=srgb&dl=pexels-pixabay-268533.jpg&fm=jpg" />
                 <main>
                     <div className='mt-[120px]'>
-                        {/* <AdDetails /> */}
-                        <h1>idddddddddddd{id}</h1>
+                        <AdDetails />
                     </div>
                 </main>
     
