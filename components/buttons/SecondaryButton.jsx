fdb1c945af1a1617d2d7db00a0093d5066e32292
escidron <eduardosanchezcidron@gmail.com>
2023-10-06 22:37:10 -0300

    implement onClick in secondaryButton component

--- components/buttons/SecondaryButton.jsx
+++ components/buttons/SecondaryButton.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 
-export default function SecondaryButton({ dark, label }) {
+export default function SecondaryButton({ dark, label,onClick }) {
     return (
         <div>
             {dark ? (
 
-                <button className='hidden xl:flex items-center z-10 h-10 border-black border-2 text-black py-[4px] px-[15px] rounded-md    hover:bg-[#FCD33B]  hover:text-black text-md'>
+                <button onClick={onClick?onClick:()=>{}} className='hidden xl:flex items-center z-10 h-10 border-black border-2 text-black py-[4px] px-[15px] rounded-md    hover:bg-[#FCD33B]  hover:text-black text-md'>
                     <p className='style_banner_button_text font-semibold text-[16px]'>{label}</p>
                 </button>
             ) : (
 
-                <button className='hidden xl:flex items-center z-10  h-10 border-[#FCD33B] border-2 text-[#FCD33B] py-[4px] px-[15px] rounded-md    hover:bg-[#FCD33B]  hover:text-black text-md'>
+                <button onClick={onClick?onClick:()=>{}} className='hidden xl:flex items-center z-10  h-10 border-[#FCD33B] border-2 text-[#FCD33B] py-[4px] px-[15px] rounded-md    hover:bg-[#FCD33B]  hover:text-black text-md'>
                     <p className='style_banner_button_text font-semibold text-[16px]'>{label}</p>
                 </button>
             )}
