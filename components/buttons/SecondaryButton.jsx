ca6f878e9de020c615b1df886af9a7c13d96a0f7
escidron <eduardosanchezcidron@gmail.com>
2023-10-06 22:42:09 -0300

    fix secondarybutton display prop on small screen

--- components/buttons/SecondaryButton.jsx
+++ components/buttons/SecondaryButton.jsx
@@ -5,12 +5,12 @@ export default function SecondaryButton({ dark, label,onClick }) {
         <div>
             {dark ? (
 
-                <button onClick={onClick?onClick:()=>{}} className='hidden xl:flex items-center z-10 h-10 border-black border-2 text-black py-[4px] px-[15px] rounded-md    hover:bg-[#FCD33B]  hover:text-black text-md'>
+                <button onClick={onClick?onClick:()=>{}} className='flex items-center z-10 h-10 border-black border-2 text-black py-[4px] px-[15px] rounded-md    hover:bg-[#FCD33B]  hover:text-black text-md'>
                     <p className='style_banner_button_text font-semibold text-[16px]'>{label}</p>
                 </button>
             ) : (
 
-                <button onClick={onClick?onClick:()=>{}} className='hidden xl:flex items-center z-10  h-10 border-[#FCD33B] border-2 text-[#FCD33B] py-[4px] px-[15px] rounded-md    hover:bg-[#FCD33B]  hover:text-black text-md'>
+                <button onClick={onClick?onClick:()=>{}} className='flex items-center z-10  h-10 border-[#FCD33B] border-2 text-[#FCD33B] py-[4px] px-[15px] rounded-md    hover:bg-[#FCD33B]  hover:text-black text-md'>
                     <p className='style_banner_button_text font-semibold text-[16px]'>{label}</p>
                 </button>
             )}
