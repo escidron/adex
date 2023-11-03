050303d164e798178398b4eddb379d686dabd1be
escidron <eduardosanchezcidron@gmail.com>
2023-11-03 14:59:16 -0300

    implement discounts for unit base listing

--- components/forms/DiscountsForm.jsx
+++ components/forms/DiscountsForm.jsx
@@ -40,6 +40,21 @@ const monthsOptions = [
     { id: 12, label: '12 months' },
 ]
 
+const unitOptions = [
+    { id: 1, label: '1 unit' },
+    { id: 2, label: '2 units' },
+    { id: 3, label: '3 units' },
+    { id: 4, label: '4 units' },
+    { id: 5, label: '5 units' },
+    { id: 10, label: '10 units' },
+    { id: 20, label: '20 units' },
+    { id: 40, label: '40 units' },
+    { id: 100, label: '100 units' },
+    { id: 150, label: '150 units' },
+    { id: 200, label: '200 units' },
+    { id: 250, label: '+250 units' },
+]
+
 export default function DiscountsForm() {
 
     const [listingProperties, setListingProperties] = useContext(ListingContext)
@@ -59,11 +74,11 @@ export default function DiscountsForm() {
         setListingProperties((prev) => ({ ...prev, discounts: [...prev.discounts, { duration: selectedMonth, discount: percentage }] }))
     }
     const removeDiscount = (id) => {
-        console.log('discounts',listingProperties.discounts)
-        const newDiscounts = listingProperties.discounts.filter((item,index) => index != id);
+        console.log('discounts', listingProperties.discounts)
+        const newDiscounts = listingProperties.discounts.filter((item, index) => index != id);
         setListingProperties((prev) => ({ ...prev, discounts: newDiscounts }))
-        
-        if(listingProperties.discounts[0].id){
+
+        if (listingProperties.discounts[0].id) {
 
             axios.post(`${process.env.NEXT_PUBLIC_SERVER_IP}/api/advertisements/delete-discount`,
                 {
@@ -80,7 +95,7 @@ export default function DiscountsForm() {
                 })
         }
     }
-    console.log('listingProperties.discounts',listingProperties.discounts)
+    console.log('listingProperties.discounts', listingProperties.discounts)
     return (
         <div className='w-full max-w-[1000px] flex flex-col items-center overflow-y-auto invisible_scroll_bar'>
             <div className='w-full flex flex-col  md:flex-row justify-between '>
@@ -112,13 +127,30 @@ export default function DiscountsForm() {
                                                 <SelectValue className='text-[12px]' placeholder="Select ..." />
                                             </SelectTrigger>
                                             <SelectContent>
-                                                <SelectGroup>
-                                                    {
-                                                        monthsOptions.map((item) => (
-                                                            <SelectItem key={item.id} value={item.id}>{item.label}</SelectItem>
-                                                        ))
-                                                    }
-                                                </SelectGroup>
+                                                {
+                                                    advertisementType === 0 && (
+
+                                                        <SelectGroup>
+                                                            {
+                                                                monthsOptions.map((item) => (
+                                                                    <SelectItem key={item.id} value={item.id}>{item.label}</SelectItem>
+                                                                ))
+                                                            }
+                                                        </SelectGroup>
+                                                    )
+                                                }
+                                                {
+                                                    advertisementType === 2 && (
+
+                                                        <SelectGroup>
+                                                            {
+                                                                unitOptions.map((item) => (
+                                                                    <SelectItem key={item.id} value={item.id}>{item.label}</SelectItem>
+                                                                ))
+                                                            }
+                                                        </SelectGroup>
+                                                    )
+                                                }
                                             </SelectContent>
                                         </Select>
                                         <div className='w-[30%] border relative px-2 rounded-md shadow-md'>
@@ -164,7 +196,18 @@ export default function DiscountsForm() {
                                         <div key={item.id}>
                                             <div className='flex gap-2 justify-between items-center p-2 w-full border mt-2 rounded-md'>
                                                 <div className='flex w-[90%]'>
-                                                    <h1 className='text-[14px]'>Buyers gets<label className='font-semibold'>{` ${item.discount}% discount `}</label>when they book for<label className='font-semibold'>{` ${item.duration} months `}</label>or more.</h1>
+                                                    {
+                                                        advertisementType === 2 && (
+
+                                                            <h1 className='text-[14px]'>Buyers gets<label className='font-semibold'>{` ${item.discount}% discount `}</label>when they book<label className='font-semibold'>{` ${item.duration} units `}</label>or more.</h1>
+                                                        )
+                                                    }
+                                                    {
+                                                        advertisementType === 0 && (
+
+                                                            <h1 className='text-[14px]'>Buyers gets<label className='font-semibold'>{` ${item.discount}% discount `}</label>when they book for<label className='font-semibold'>{` ${item.duration} months `}</label>or more.</h1>
+                                                        )
+                                                    }
                                                 </div>
                                                 <div onClick={() => removeDiscount(item.id ? item.id : index)} className='cursor-pointer w-[10%] flex justify-center items-center' >
                                                     <Trash size={20} />
@@ -192,6 +235,24 @@ export default function DiscountsForm() {
                                 <p>• 3 months earns the buyer a 10% discount,</p>
                                 <p>• 6 months earns the buyer a 15% discount, and </p>
                                 <p>• 12 months ears the buyer a 20% discount. </p>
+                            </CardContent>
+                        </Card>
+                    )
+                }
+                {
+                    advertisementType === 2 && (
+                        <Card className='w-full mt-[50px] md:max-w-[450px] h-[260px] md:mt-0'>
+                            <CardHeader>
+                                <CardTitle className='flex gap-2 items-center'>
+                                    <Info />
+                                    Quantity Discount
+                                </CardTitle>
+                                <CardDescription>Much like many (if not all) of the big companies, ADEX encourages you to offer discounts. This incentivizes buyers to book more units. For example: </CardDescription>
+                            </CardHeader>
+                            <CardContent>
+                                <p>• 5 units earns the buyer a 10% discount,</p>
+                                <p>• 6 units earns the buyer a 15% discount, and </p>
+                                <p>• 12 units ears the buyer a 20% discount. </p>
                             </CardContent>
                         </Card>
                     )
