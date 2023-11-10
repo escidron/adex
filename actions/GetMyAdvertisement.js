ef3247c4090591c5b8ddb490b658d08c91dd946e
escidron <eduardosanchezcidron@gmail.com>
2023-11-10 17:47:21 -0300

    debug token error 3

--- actions/GetMyAdvertisement.js
+++ actions/GetMyAdvertisement.js
@@ -3,38 +3,40 @@ import axios from "axios";
 export default async function GetMyAdvertisement(token, id) {
   console.log("getadversitmetoken", token);
   try {
-    // const response = await fetch(
-    //   `${process.env.NEXT_PUBLIC_SERVER_IP}/api/advertisements/my-advertisement`,
-    //   { id: id },
-    //   {
-    //     withCredentials: true,
-    //     headers: {
-    //       Cookie: `jwt=${token}`,
-    //     },
-    //   }
-    // );
-
-    // const myListing = response.data.data[0];
-    // return myListing;
-
-    const response = await fetch(
+    const response = await axios.post(
       `${process.env.NEXT_PUBLIC_SERVER_IP}/api/advertisements/my-advertisement`,
+      { id: id },
       {
-        method: "POST",
+        withCredentials: true,
         headers: {
-          Cookie: `jwt=${token}`,
+          Authorization: `${token}`,
         },
-        credentials: "include",
-        body: JSON.stringify({ id: id }),
       }
     );
 
-    if (response.status === 200) {
-      const myListing = await response.json();
-      return myListing;
-    } else {
-      return null;
-    }
+    const myListing = response.data.data[0];
+    return myListing;
+
+    // const response = await fetch(
+    //   `${process.env.NEXT_PUBLIC_SERVER_IP}/api/advertisements/my-advertisement`,
+    //   {
+    //     method: "POST",
+    //     headers: {
+    //       Cookie: `jwt=${token}`,
+    //       'Accept': 'application/json',
+    //       'Content-Type': 'application/json'        
+    //     },
+    //     credentials: "include",
+    //     body: JSON.stringify({ id: id }),
+    //   }
+    // );
+
+    // if (response.status === 200) {
+    //   const myListing = await response.json();
+    //   return myListing;
+    // } else {
+    //   return null;
+    // }
   } catch (error) {
     console.log(error);
     return null;
