2b3e021faf0f3188016ba6a5dd0e85ced3c994f9
escidron <eduardosanchezcidron@gmail.com>
2023-11-12 13:55:36 -0300

    fix seller image error

--- app/market-place/details/_components/SellerDetails.jsx
+++ app/market-place/details/_components/SellerDetails.jsx
@@ -4,12 +4,13 @@ import StarRoundedIcon from '@mui/icons-material/StarRounded'
 import Link from "next/link"
 
 import { Button } from "@/components/ui/button"
-import { Image } from "lucide-react"
 import { useRouter } from "next/navigation"
+import Image from 'next/image'
 
 
 export default function SellerDetails({ listingProperties }) {
     const router = useRouter()
+    console.log('listingProperties.seller_image', listingProperties.seller_image)
     return (
         <div>
             {
@@ -18,12 +19,13 @@ export default function SellerDetails({ listingProperties }) {
                         <div className='flex flex-col gap-2'>
                             <p className='text-[26px]'>Seller</p>
                             <div className="flex items-center">
+
                                 <Link href={`/market-place/${listingProperties.company_id ? 'company-details' : 'seller-details'}?id=${listingProperties.company_id ? listingProperties.company_id : listingProperties.seller_id}`} className='w-[50px] h-[50px] cursor-pointer flex justify-center items-center'>
                                     {
                                         listingProperties.seller_image ? (
 
                                             <Image
-                                                src={listingProperties.seller_image && !listingProperties.company_id ? listingProperties.seller_image : listingProperties.company_id ? company.company_logo : '/nouser.png'}
+                                                src={listingProperties.seller_image}
                                                 alt="Seller Logo"
                                                 priority
                                                 width={2000}
@@ -40,11 +42,11 @@ export default function SellerDetails({ listingProperties }) {
                                 <div className='ml-2'>
                                     <p className="text-[24px]">{listingProperties.seller_name}</p>
                                     <div className="flex items-center justify-start">
-                                        <StarRoundedIcon  sx={{ color: '#FCD33B',fontSize:'16px' }} />
-                                        <StarRoundedIcon  sx={{ color: '#FCD33B',fontSize:'16px' }} />
-                                        <StarRoundedIcon  sx={{ color: '#FCD33B',fontSize:'16px' }} />
-                                        <StarRoundedIcon  sx={{ color: 'gray',fontSize:'16px' }} />
-                                        <StarRoundedIcon  sx={{ color: 'gray',fontSize:'16px' }} />
+                                        <StarRoundedIcon sx={{ color: '#FCD33B', fontSize: '16px' }} />
+                                        <StarRoundedIcon sx={{ color: '#FCD33B', fontSize: '16px' }} />
+                                        <StarRoundedIcon sx={{ color: '#FCD33B', fontSize: '16px' }} />
+                                        <StarRoundedIcon sx={{ color: 'gray', fontSize: '16px' }} />
+                                        <StarRoundedIcon sx={{ color: 'gray', fontSize: '16px' }} />
                                     </div>
                                 </div>
                                 <Button onClick={() => router.push(`/market-place/${listingProperties.company_id ? 'company-details' : 'seller-details'}?id=${listingProperties.company_id ? listingProperties.company_id : listingProperties.seller_id}`)} size='sm' variant='outline' className='ml-auto'>Seller Details</Button>
