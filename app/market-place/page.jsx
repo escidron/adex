24b71898ba3645b82421a75b63da4419c1d3c213
escidron <eduardosanchezcidron@gmail.com>
2023-11-19 23:30:27 -0300

    fix loading state market place

--- app/market-place/page.jsx
+++ app/market-place/page.jsx
@@ -41,12 +41,15 @@ export default function MarketPlace() {
         setNewData([])
         const response = await GetFilteredAdvertisements(type, adGroup, priceMin, priceMax, key)
         setAllData(response)
+        setIsDataLoaded(true);
+
       }
 
       if(allData.length == 0){
         getAds();
+      }else{
+        setIsDataLoaded(true);
       }
-      setIsDataLoaded(true);
 
     } else {
       if (navigator.geolocation) {
