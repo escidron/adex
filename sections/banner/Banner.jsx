91bf30e814941d6fa31c9f38c874e7ab6a160e3f
escidron <eduardosanchezcidron@gmail.com>
2023-11-28 23:30:10 -0300

    load faster the banner image

--- sections/banner/Banner.jsx
+++ sections/banner/Banner.jsx
@@ -4,6 +4,7 @@ import { useState } from "react"
 import { useRouter } from 'next/navigation';
 import { Loader2 } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 
 
@@ -17,7 +18,15 @@ export default function Banner() {
 
 
       <div className='absolute top-0 left-0 w-full h-[100vh]  bg-black z-90 opacity-20'></div>
-      <div className='style_banner'></div>
+      {/* <div className='style_banner'></div> */}
+      <Image
+        src='/adex-home-banner.jpg'
+        alt="banner image"
+        priority ={true}
+        width={3000}
+        height={3000}
+        className=' h-full w-full object-cover mt-[80px]'
+      />
       <div className=' max-h-[350px] text-[34px] w-[90%] flex flex-col justify-center items-center rounded-md p-8 max-w-[400px] bg-white z-8  absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]
                         md:w-[70%] md:max-w-[600px]
                         lg:max-w-[550px] lg:h-[450px] lg:top-[30%] lg:left-[10%] lg:translate-x-0 lg:translate-y-0' >
@@ -26,7 +35,7 @@ export default function Banner() {
 
 
         <div className="mt-10 ">
-          <Button className='h-[60px] text-[20px]' disabled={isPending} size='lg' onClick={()=>{
+          <Button className='h-[60px] text-[20px]' disabled={isPending} size='lg' onClick={() => {
             setIsPending(true)
             router.push('/market-place')
           }}>
