1d05b446a608149fa9c5a5de489b75472a2fd8bd
escidron <eduardosanchezcidron@gmail.com>
2023-12-01 11:34:25 -0300

    fix the name of the seller before load the page

--- app/market-place/seller-details/page.jsx
+++ app/market-place/seller-details/page.jsx
@@ -39,110 +39,114 @@ export default function SellerDetailsPage() {
         };
         handleRouteChange()
     }, []);
-    console.log('seller details',seller)
+    console.log('seller details', seller)
     return (
         <>
-        
-        <div className={`mt-[150px] w-full h-full flex justify-center items-center `}>
-            <div className='flex flex-col w-[80%] max-w-[1000px] items-center'>
 
-                {
-                    contentIsLoaded ? (
-                        <div className="flex gap-6 items-center justify-center border shadow-md py-8 px-4 rounded-lg w-[70%]">
-                            <div className='w-[150px] h-[150px] min-w-[150px] cursor-pointer'>
-                                <Image
-                                    src={seller.image ? seller.image : '/nouser.png'}
-                                    alt="Seller Logo"
-                                    priority
-                                    width={2000}
-                                    height={2000}
-                                    className='rounded-full w-full h-full object-cover'
-                                />
-                                <div className="flex items-center justify-center">
-                                    <StarRoundedIcon fontSize='small' sx={{ color: '#FCD33B' }} />
-                                    <StarRoundedIcon fontSize='small' sx={{ color: '#FCD33B' }} />
-                                    <StarRoundedIcon fontSize='small' sx={{ color: '#FCD33B' }} />
-                                    <StarRoundedIcon fontSize='small' sx={{ color: 'gray' }} />
-                                    <StarRoundedIcon fontSize='small' sx={{ color: 'gray' }} />
+            <div className={`mt-[150px] w-full h-full flex justify-center items-center `}>
+                <div className='flex flex-col w-[80%] max-w-[1000px] items-center'>
+
+                    {
+                        contentIsLoaded ? (
+                            <div className="flex gap-6 items-center justify-center border shadow-md py-8 px-4 rounded-lg w-[70%]">
+                                <div className='w-[150px] h-[150px] min-w-[150px] cursor-pointer'>
+                                    <Image
+                                        src={seller.image ? seller.image : '/nouser.png'}
+                                        alt="Seller Logo"
+                                        priority
+                                        width={2000}
+                                        height={2000}
+                                        className='rounded-full w-full h-full object-cover'
+                                    />
+                                    <div className="flex items-center justify-center">
+                                        <StarRoundedIcon fontSize='small' sx={{ color: '#FCD33B' }} />
+                                        <StarRoundedIcon fontSize='small' sx={{ color: '#FCD33B' }} />
+                                        <StarRoundedIcon fontSize='small' sx={{ color: '#FCD33B' }} />
+                                        <StarRoundedIcon fontSize='small' sx={{ color: 'gray' }} />
+                                        <StarRoundedIcon fontSize='small' sx={{ color: 'gray' }} />
+                                    </div>
+                                </div>
+                                <div className='flex flex-col justify-start h-auto' >
+                                    <h1 className='text-[35px]'>{`${seller.name}`}</h1>
+                                    {
+                                        seller.handle_is_public == "1" && seller.handle && (
+                                            <div className='mt-1 flex items-center gap-3'>
+                                                <AccountCircleIcon sx={{ fontSize: '18px', color: 'gray' }} />
+                                                <h1>{seller.handle}</h1>
+                                            </div>
+
+                                        )
+                                    }
+                                    {
+                                        seller.city_is_public == '1' && seller.city && (
+
+                                            <div className='mt-1 flex items-center gap-3'>
+                                                <LocationOnIcon sx={{ fontSize: '18px', color: 'gray' }} />
+                                                <h1>{seller.city}</h1>
+                                            </div>
+                                        )
+                                    }
+                                    {
+                                        seller.profession_is_public == '1' && seller.profession && (
+
+                                            <div className='mt-1 flex items-center gap-3'>
+                                                <WorkIcon sx={{ fontSize: '18px', color: 'gray' }} />
+                                                <h1>{seller.profession}</h1>
+                                            </div>
+                                        )
+                                    }
+                                    {
+                                        seller.bio_is_public == '1' && (
+
+                                            <p className='mt-2 max-w-full'>{seller.bio != 'null' ? seller.bio : ''}</p>
+                                        )
+                                    }
                                 </div>
                             </div>
-                            <div className='flex flex-col justify-start h-auto' >
-                                <h1 className='text-[35px]'>{`${seller.name}`}</h1>
-                                {
-                                    seller.handle_is_public == "1" && seller.handle && (
-                                        <div className='mt-1 flex items-center gap-3'>
-                                            <AccountCircleIcon sx={{ fontSize: '18px', color: 'gray' }} />
-                                            <h1>{seller.handle}</h1>
-                                        </div>
+                        ) : (
+                            <SellerCardSkeleton />
+                        )
+                    }
 
-                                    )
-                                }
-                                {
-                                    seller.city_is_public == '1' && seller.city && (
 
-                                        <div className='mt-1 flex items-center gap-3'>
-                                            <LocationOnIcon sx={{ fontSize: '18px', color: 'gray' }} />
-                                            <h1>{seller.city}</h1>
-                                        </div>
-                                    )
-                                }
-                                {
-                                    seller.profession_is_public == '1' && seller.profession && (
 
-                                        <div className='mt-1 flex items-center gap-3'>
-                                            <WorkIcon sx={{ fontSize: '18px', color: 'gray' }} />
-                                            <h1>{seller.profession}</h1>
-                                        </div>
-                                    )
-                                }
-                                {
-                                    seller.bio_is_public == '1' && (
+                    <Divider variant="" sx={{ color: 'black', width: '100%', marginTop: '40px', marginBottom: '40px' }} />
 
-                                        <p className='mt-2 max-w-full'>{seller.bio != 'null' ? seller.bio : ''}</p>
-                                    )
-                                }
+
+                    {
+                        contentIsLoaded && (
+
+                            <div>
+                                <p className='text-[32px]'>{`${seller.name}’s Listings`}</p>
                             </div>
-                        </div>
-                    ) : (
-                        <SellerCardSkeleton />
-                    )
-                }
+                        )
+                    }
 
+                    {
+                        !contentIsLoaded && (
+                            <div className='w-full h-auto mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-2 2xl:grid-cols-3 gap-4 md:p-2 xl:p-[10px]'>
 
+                                <MarketPlaceCardSkeleton />
+                                <MarketPlaceCardSkeleton />
+                                <MarketPlaceCardSkeleton />
+                            </div>
+                        )
+                    }
 
-                <Divider variant="" sx={{ color: 'black', width: '100%', marginTop: '40px', marginBottom: '40px' }} />
+                    {
+                        sellerListings.length > 0 && contentIsLoaded && (
 
+                            <div className={` h-auto mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-2 2xl:grid-cols-3 gap-4 md:p-2 xl:p-[10px] `}>
+                                {sellerListings.map((ad) => (
+                                    <MarketPlaceCard key={ad.id} ad={ad} />
+                                ))}
+                            </div>
+                        )
+                    }
 
-
-                <div>
-                    <p className='text-[32px]'>{`${seller.name}’s Listings`}</p>
-                </div>
-
-                {
-                    !contentIsLoaded && (
-                        <div className='w-full h-auto mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-2 2xl:grid-cols-3 gap-4 md:p-2 xl:p-[10px]'>
-
-                            <MarketPlaceCardSkeleton />
-                            <MarketPlaceCardSkeleton />
-                            <MarketPlaceCardSkeleton />
-                        </div>
-                    )
-                }
-
-                {
-                    sellerListings.length > 0 && contentIsLoaded && (
-
-                        <div className={` h-auto mt-8 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-2 2xl:grid-cols-3 gap-4 md:p-2 xl:p-[10px] `}>
-                            {sellerListings.map((ad) => (
-                                <MarketPlaceCard key={ad.id} ad={ad} />
-                            ))}
-                        </div>
-                    )
-                }
-
-            </div >
-        </div>
-        <Footer />
+                </div >
+            </div>
+            <Footer />
         </>
 
     )
