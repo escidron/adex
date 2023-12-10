f64ad5cb88f77999362f81b0a179d18319900795
escidron <eduardosanchezcidron@gmail.com>
2023-12-10 12:31:37 -0300

    add back button in the reservation modal for company

--- components/reservation/Reservation.jsx
+++ components/reservation/Reservation.jsx
@@ -13,7 +13,7 @@ import axios from 'axios';
 import formatNumberInput from '@/utils/formatInputNumbers';
 import toast from "react-hot-toast";
 import { UserContext } from '../../app/layout';
-import { HelpCircle, Loader2 } from 'lucide-react';
+import { ArrowLeft, HelpCircle, Loader2, X } from 'lucide-react';
 import { Button } from '../ui/button';
 import { DateFieldComponent } from '../datePicker/DateFieldComponent';
 
@@ -160,7 +160,6 @@ export default function Reservation({ data, hasCard, setShowModal, setIsBooked, 
             </div>
         )
     }
-    console.log('companies', companies)
     return (
         <div className={`w-[350px] h-fit flex flex-col   shadow-lg rounded-lg border p-4 `}>
             {data.price && (
@@ -241,7 +240,7 @@ export default function Reservation({ data, hasCard, setShowModal, setIsBooked, 
             {
                 data.status == '1' && user.isLogged && (
 
-                    <Dialog className='w-full '>
+                    <Dialog className='w-full ' onOpenChange={() => setHasCompanySelected(false)}>
                         <DialogTrigger className='w-full mt-2 h-10 px-4 py-2 bg-black text-primary-foreground hover:bg-black/90 inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50'>
                             Request Booking
                         </DialogTrigger>
@@ -258,13 +257,13 @@ export default function Reservation({ data, hasCard, setShowModal, setIsBooked, 
                                         <div className='max-h-[400px] overflow-y-auto invisible_scroll_bar'>
                                             <CompanyList
                                                 companies={companies}
-                                                handleSelectedCompany={(id)=>handleSelectedCompany(id)}
+                                                handleSelectedCompany={(id) => handleSelectedCompany(id)}
                                                 selectedCompany={selectedCompany}
                                             />
                                         </div>
                                         <DialogFooter className='mt-4'>
                                             <div className='w-full flex justify-end items-center'>
-                                                <Button disabled={!selectedCompany} type='submit' onClick={()=>setHasCompanySelected(true)}>
+                                                <Button disabled={!selectedCompany} type='submit' onClick={() => setHasCompanySelected(true)}>
                                                     {isPending && <Loader2 size={15} className="animate-spin mr-2" />}
                                                     Next
                                                 </Button>
@@ -276,7 +275,12 @@ export default function Reservation({ data, hasCard, setShowModal, setIsBooked, 
                             {
                                 (paymentMethods.length == 0 || addCard) && (isCompanyProfile ? (hasCompanySelected ? true : false) : true) && (
                                     <>
-                                        <DialogHeader>
+                                        {
+                                            isCompanyProfile && (
+                                                <ArrowLeft onClick={() => setHasCompanySelected(false)} className="h-4 w-4 absolute left-4 top-4 cursor-pointer rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground" />
+                                            )
+                                        }
+                                        <DialogHeader className='mt-6'>
                                             <DialogTitle>Add Card Details</DialogTitle>
                                             <DialogDescription>
                                                 You will only be charge if your request is approved.
@@ -289,18 +293,23 @@ export default function Reservation({ data, hasCard, setShowModal, setIsBooked, 
                                                 companyId={selectedCompany}
                                             />
                                         </Elements>
-                                        {
+                                        {/* {
                                             isCompanyProfile && (
-                                                <Button className='mt-[-10px]' variant='outline' onClick={()=>setHasCompanySelected(false)}>Select Company</Button>
+                                                <Button className='mt-[-10px]' variant='outline' onClick={() => setHasCompanySelected(false)}>Select Company</Button>
                                             )
-                                        }
+                                        } */}
                                     </>
                                 )
                             }
                             {
                                 (paymentMethods.length > 0 && !addCard) && (isCompanyProfile ? (hasCompanySelected ? true : false) : true) && (
                                     <>
-                                        <DialogHeader>
+                                        {
+                                            isCompanyProfile && (
+                                                <ArrowLeft onClick={() => setHasCompanySelected(false)} className="h-4 w-4 absolute left-4 top-4 cursor-pointer rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground" />
+                                            )
+                                        }
+                                        <DialogHeader className='mt-6'>
                                             <DialogTitle>Confirm the Payment Method</DialogTitle>
                                             <DialogDescription>
                                                 You will only be charge if your request is approved.
