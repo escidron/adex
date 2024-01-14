f2900199e96b9d1baf1b1605524c239e153f2219
escidron <eduardosanchezcidron@gmail.com>
2024-01-14 13:48:35 -0300

    add no reviews label in the reviews carrousel

--- app/market-place/seller-details/_components/ReviewsCarrousel.jsx
+++ app/market-place/seller-details/_components/ReviewsCarrousel.jsx
@@ -1,32 +1,41 @@
 import { Card, CardContent } from "@/components/ui/card"
 import {
-    Carousel,
-    CarouselContent,
-    CarouselItem,
-    CarouselNext,
-    CarouselPrevious,
-  } from "@/components/ui/carousel"
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel"
 import ReviewCard from "../../details/_components/ReviewCard"
-export default function ReviewsCarrousel({data}) {
-    return (
-        <Carousel
-        opts={{
-          align: "start",
-        }}
-        className="w-full "
-      >
-        <CarouselContent>
-          {data.map((review, index) => (
-            <CarouselItem key={index} className="md:basis-1/2 ">
-              <div className="p-1">
-                <ReviewCard key={review.id} review={review} />
-              </div>
-            </CarouselItem>
-          ))}
-        </CarouselContent>
-        <CarouselPrevious />
-        <CarouselNext />
-      </Carousel>
+export default function ReviewsCarrousel({ data }) {
+  return (
+    <>
 
-    )
+      {
+        data.length > 0 ? (
+
+          <Carousel
+            opts={{
+              align: "start",
+            }}
+            className="w-full "
+          >
+            <CarouselContent>
+              {data.map((review, index) => (
+                <CarouselItem key={index} className="md:basis-1/2 ">
+                  <div className="p-1">
+                    <ReviewCard key={review.id} review={review} />
+                  </div>
+                </CarouselItem>
+              ))}
+            </CarouselContent>
+            <CarouselPrevious />
+            <CarouselNext />
+          </Carousel>
+        ) : (
+          <p className='text-gray-600 italic text-[26px]'>No reviews</p>
+          )
+      }
+    </>
+  )
 }
