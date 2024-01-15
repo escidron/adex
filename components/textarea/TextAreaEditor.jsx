2181d46a722df39b5a47fe2ccb814627456554a7
escidron <eduardosanchezcidron@gmail.com>
2024-01-15 19:23:14 -0300

    rollback in the autofocus of the quill component

--- components/textarea/TextAreaEditor.jsx
+++ components/textarea/TextAreaEditor.jsx
@@ -1,38 +1,49 @@
 "use client";
 
+// import ReactQuill from "react-quill"
 import dynamic from "next/dynamic";
 import { useEffect, useMemo, useRef } from "react";
 import "react-quill/dist/quill.snow.css";
-// import ReactQuill from "react-quill"
 
-export const TextAreaEditor = ({onChange,value}) => {
-  const ReactQuill = useMemo(() => dynamic(() => import("react-quill"), { ssr: false }), []);
+export const TextAreaEditor = ({ onChange, value }) => {
+   const ReactQuill = useMemo(() => dynamic(() => import("react-quill"), { ssr: false }), []);
+
 
   var toolbarOptions = [
-    
+
     ['bold', 'italic', 'underline'],        // toggled buttons
-  
-    [{ 'list': 'ordered'}, { 'list': 'bullet' }],
-  
-  
+
+    [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+
+
   ];
 
-  const refQuill = useRef(null)
+  // const refQuill = useRef(null)
 
-  useEffect(() => {
-    refQuill.current.focus()
-  }, []);
+  // useEffect(() => {
+  //   if (refQuill) {
+  //     console.log('reaactquil', refQuill)
+  //     refQuill.current.focus()
+  //   }
+  // }, []);
+
 
   return (
-    <div className="bg-white h-[100px]">
-      <ReactQuill
-        theme="snow"
-        ref={refQuill}
-        value={value}
-        onChange={onChange}
-        modules={{toolbar:toolbarOptions}}
-        selection={{start:0, end:0}}
-      />
-    </div>
-  );
+    <>
+      {
+        ReactQuill && (
+          <div className="bg-white h-[100px]">
+            <ReactQuill
+              theme="snow"
+              // ref={refQuill}
+              value={value}
+              onChange={onChange}
+              modules={{ toolbar: toolbarOptions }}
+            />
+          </div>
+        )
+      }
+    </>
+  )
+
 };
