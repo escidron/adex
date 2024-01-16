5078f8b51c38a906ba0d25fa57d93318cfc93cba
escidron <eduardosanchezcidron@gmail.com>
2024-01-16 14:26:29 -0300

    add description inthe use cases

--- sections/useCases/useCasesData.js
+++ sections/useCases/useCasesData.js
@@ -4,24 +4,24 @@ export const useCasesData = [
         id:1,
         title:'Title 1',
         image:'use-case-1.png',
-        description:'ADEX empowers restaurants to monetize unused spaces by offering them as advertising real estate, enhancing revenue streams.'
+        description:'ADEX empowers restaurants to monetize unused spaces by offering them as advertising real estate.'
     },
     {
         id:2,
         title:'Title 2',
         image:'use-case-2.png',
-        description:''
+        description:'ADEX empowers restaurants to monetize unused spaces by offering them as advertising real estate.'
     },
     {
         id:3,
         title:'Title 3',
         image:'use-case-3.png',
-        description:''
+        description:'ADEX empowers restaurants to monetize unused spaces by offering them as advertising real estate.'
     },
     {
         id:4,
         title:'Title 4',
         image:'use-case-4.png',
-        description:'ADEX empowers restaurants to monetize unused spaces by offering them as advertising real estate, enhancing revenue streams.'
+        description:'ADEX empowers restaurants to monetize unused spaces by offering them as advertising real estate.'
     }
 ];
