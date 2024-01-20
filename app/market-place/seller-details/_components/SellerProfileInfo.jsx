b1349c1586da9fca314fb990aadce9302a0b0c99
escidron <eduardosanchezcidron@gmail.com>
2024-01-20 15:13:47 -0300

    fix the user bio in the seller details page

--- app/market-place/seller-details/_components/SellerProfileInfo.jsx
+++ app/market-place/seller-details/_components/SellerProfileInfo.jsx
@@ -3,19 +3,30 @@ import WorkIcon from '@mui/icons-material/Work';
 import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 import Image from 'next/image';
 import RatingComponent from '@/components/rating/RatingComponent';
+import { Preview } from '@/components/textarea/TextAreaReader';
 
 export default function SellerProfileInfo({ seller }) {
     return (
-        <div className="flex flex-col mx-auto md:flex-row gap-6 items-center justify-center border shadow-md py-8 px-4 rounded-lg w-fit lg:w-[70%]">
-            <div className='w-[150px] h-[150px] min-w-[150px] cursor-pointer'>
-                <Image
-                    src={seller.image ? seller.image : '/nouser.png'}
-                    alt="Seller Logo"
-                    priority
-                    width={2000}
-                    height={2000}
-                    className='rounded-full w-full h-full object-cover'
-                />
+        <div className="flex flex-col mx-auto md:flex-row gap-6 items-center justify-center sm:items-center sm:justify-start border shadow-md py-8 px-4 rounded-lg w-fit lg:w-[70%]">
+            <div className='w-[150px] h-[150px] min-w-[150px] cursor-pointer flex flex-col items-center'>
+
+                {
+                    seller.image ? (
+
+                        <Image
+                            src={seller.image}
+                            alt="seller Logo"
+                            priority
+                            width={2000}
+                            height={2000}
+                            className='rounded-full w-full h-full object-cover'
+                        />
+                    ) : (
+                        <div className="bg-black text-[28px] text-white font-[600] rounded-full min-w-[120px] w-[120px] h-[120px] flex justify-center items-center">
+                            {seller.name.substring(0, 1).toUpperCase()}
+                        </div>
+                    )
+                }
                 <div className="flex items-center justify-center">
                     <RatingComponent readOnly={true} rating={seller.rating} />
                 </div>
@@ -52,7 +63,9 @@ export default function SellerProfileInfo({ seller }) {
                 {
                     seller.bio_is_public == '1' && (
 
-                        <p className='mt-2 max-w-full'>{seller.bio != 'null' ? seller.bio : ''}</p>
+                        // <p className='mt-2 max-w-full'>{seller.bio != 'null' ? seller.bio : ''}</p>
+                        <Preview value={seller.bio != 'null' ? seller.bio : ''} heigth={200} autoHeigth={true} />
+
                     )
                 }
             </div>
