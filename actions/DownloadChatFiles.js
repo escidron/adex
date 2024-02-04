46a836b7b5dad3b983c8d51a270160945c458b5c
escidron <eduardosanchezcidron@gmail.com>
2024-02-04 10:09:48 -0300

    fix download files cause http problems

--- actions/DownloadChatFiles.js
+++ actions/DownloadChatFiles.js
@@ -1,24 +1,55 @@
-export default async function DownloadChatFiles(files) {
-  try {
+// export default async function DownloadChatFiles(files) {
+//   try {
 
-    files.forEach(async file => {
-      
-      console.log(`fgiles`,file)
-      const response = await fetch(file);
+//     files.forEach(async file => {
+
+//       console.log(`fgiles`,file)
+//       const response = await fetch(file);
+//       const blob = await response.blob();
+
+//       const url = window.URL.createObjectURL(blob);
+//       const link = document.createElement('a');
+//       link.href = url;
+//       link.setAttribute('download', file);
+//       document.body.appendChild(link);
+//       link.click();
+//       document.body.removeChild(link);
+//     });
+
+//   } catch (error) {
+//     console.error('Erro ao baixar o arquivo:', error);
+//   }
+// };
+export default async function DownloadChatFiles(files) {
+  console.log(`entrou`)
+  try {
+    files.forEach(async (file) => {
+      // const fileName = `listing_403.png`
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_SERVER_IP}/api/advertisements/download-files`,
+        {
+          method: "POST",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+          },
+          credentials: "include",
+          body: JSON.stringify({
+            file: file
+          }),
+        }
+      );
       const blob = await response.blob();
-  
+
       const url = window.URL.createObjectURL(blob);
-      const link = document.createElement('a');
+      const link = document.createElement("a");
       link.href = url;
-      link.setAttribute('download', file);
+      link.setAttribute("download", file);
       document.body.appendChild(link);
       link.click();
       document.body.removeChild(link);
     });
-
   } catch (error) {
-    console.error('Erro ao baixar o arquivo:', error);
+    console.error("Erro ao baixar o arquivo:", error);
   }
-};
-  
-  
+}
--- utils/getFilesLink.js
+++ utils/getFilesLink.js
@@ -1,12 +1,12 @@
 export default function getFilesLink(files){
-    const  FilesArray = files  ? files.split(`;`) : []
-    const serverUrl = process.env.NEXT_PUBLIC_SERVER_IP; 
-    const httpImageUrl = serverUrl.replace("https://", "http://");
+    const  filesArray = files  ? files.split(`;`) : []
+    // const serverUrl = process.env.NEXT_PUBLIC_SERVER_IP; 
+    // const httpImageUrl = serverUrl.replace("https://", "http://");
     
-    const filesLink = FilesArray.map(file=>{
+    // const filesLink = FilesArray.map(file=>{
 
-        return `${httpImageUrl}/images/files/${file}`
-    })
+    //     return `${httpImageUrl}/images/files/${file}`
+    // })
 
-    return filesLink
+    return filesArray
 }
