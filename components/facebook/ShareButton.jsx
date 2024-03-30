9d78cb094b992b9cae1341f2b1a9db6417ad59a2
escidron <eduardosanchezcidron@gmail.com>
2024-03-29 22:24:12 -0300

    clean new code it does not work

--- components/facebook/ShareButton.jsx
+++ components/facebook/ShareButton.jsx
@@ -1,32 +1,8 @@
 'use client'
-import { FacebookProvider, ShareButton, useFacebook } from 'react-facebook';
+import {  ShareButton } from 'react-facebook';
 import { Facebook } from 'lucide-react';
-import { useEffect } from 'react';
 
 export default function ShareButtonFacebook({ id }) {
-  const { isLoading, init, error } = useFacebook();
-
-  useEffect(() => {
-
-
-    async function refresh() {
-      const api = await init();
-      console.log('api', api)
-      try {
-        // Realiza a solicitação de limpeza de cache
-        const res = await api.getFB().api('https://graph.facebook.com/','post',{
-            id: `https://adexconnect.com/my-listing/sharing-listing/${id}`,
-            scrape: true
-          }
-        );
-          console.log('res',res)
-      } catch (error) {
-        console.error('Erro ao limpar o cache do scraper do Facebook:', error);
-      }
-    }
-    refresh()
-  }, []);
-
 
   return (
     <>
