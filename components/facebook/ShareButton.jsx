8615a2f66acf884faa060d808bd6d8a48695c1bc
escidron <eduardosanchezcidron@gmail.com>
2024-03-29 21:04:45 -0300

    trying to clear facebook cache

--- components/facebook/ShareButton.jsx
+++ components/facebook/ShareButton.jsx
@@ -5,7 +5,7 @@ export default function ShareButtonFacebook({id}) {
   return (
     <>
       <FacebookProvider appId="1611678826026608" >
-        <ShareButton href={`https://adexconnect.com/my-listing/sharing-listing/${id}`}
+        <ShareButton href={`https://adexconnect.com/my-listing/sharing-listing/${id}?timestamp=${Date.now()}`}
         >
           <div className='w-[180px] flex gap-3 border p-3 mt-2 bg-white shadow-sm rounded-lg cursor-pointer hover:border-black'>
             <Facebook />
