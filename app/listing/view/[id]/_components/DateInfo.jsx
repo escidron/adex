c64082e60f5a59abedcada6e9233c5d6d5dfd799
escidron <eduardosanchezcidron@gmail.com>
2024-03-31 22:21:58 -0300

    fix date format

--- app/listing/view/[id]/_components/DateInfo.jsx
+++ app/listing/view/[id]/_components/DateInfo.jsx
@@ -46,7 +46,7 @@ export default function DateInfo({ listingProperties }) {
                                 className="w-[240px] justify-start text-left font-normal"
                             >
                                 <CalendarIcon className="mr-2 h-4 w-4" />
-                                {format(parseISO(listingProperties.first_available_date), "PPP")}
+                                {format(new Date(listingProperties.first_available_date), "PPP")}
                             </Button>
                         </div>
                     </>
