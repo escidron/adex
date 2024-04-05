4ade9df1fef0616299ec0485371864060cf172a6
escidron <eduardosanchezcidron@gmail.com>
2024-04-05 20:32:59 -0300

    implement qr code new layout

--- app/listing/view/[id]/_components/QrCodeComponent.jsx
+++ app/listing/view/[id]/_components/QrCodeComponent.jsx
@@ -3,7 +3,7 @@ import QRCode from "react-qr-code";
 export default function QrCodeComponent({ value }) {
   return (
     <div className="p-4 bg-white border rounded-md mt-[40px]">
-        <QRCode value={value} size={150}/>
+        <QRCode value={value} size={350}/>
     </div>
   )
 }
--- app/listing/view/[id]/_components/QrCodeInfo.jsx
+++ app/listing/view/[id]/_components/QrCodeInfo.jsx
@@ -25,16 +25,29 @@ export default function QrCodeInfo({ id }) {
             <p className='text-[26px]'>QR Code</p>
             <p className='mt-1'>Showcase your listing by printing and attaching it to your asset.</p>
 
-            <div ref={componentRef} className='mx-auto w-full max-w-[700px] mt-8  flex flex-col items-center border text-white p-[50px]'>
-
-                <Image
-                    src='/adex-logo-black-yellow.png'
-                    alt="Adex Logo"
-                    width={70}
-                    height={70}
-                />
-                <p className='mt-1'>Reach more people, place your ad here.</p>
-                <QrCodeComponent value={`https://adexconnect.com/market-place/details?id=${id}`} />
+            <div ref={componentRef} className='mx-auto w-full max-w-[700px] mt-8  flex flex-col items-center  text-white p-[50px]'>
+                <div className='relative rounded-lg border border-black p-4 w-full flex flex-col items-center'>
+                    <Image
+                        src='/qrcode-header.png'
+                        alt="Adex Logo"
+                        width={2000}
+                        height={2000}
+                        className='w-[365px] max-h-[185px]'
+                    />
+                    <p className='mt-1'>Reach more people, place your ad here.</p>
+                    <QrCodeComponent value={`https://adexconnect.com/market-place/details?id=${id}`} />
+                    <p className='text-black font-[500] mt-3'>Scan this code now with your phone</p>
+                    <p className='text-black font-[500] pb-[50px]'>to book this space using <b className='text-[20px]'>adexconnect.com</b></p>
+                    <div className='absolute bottom-[-30px] bg-white px-2 flex gap-2 items-center'>
+                        <Image
+                            src='/adex-logo-black-yellow.png'
+                            alt="Adex Logo"
+                            width={70}
+                            height={70}
+                        />
+                        <p className='text-black'>Advertisement Reimagined...</p>
+                    </div>
+                </div>
             </div>
             <Button onClick={handleDownload} className='mt-6 gap-2'>
                 <ArrowDownToLine size={16} />
